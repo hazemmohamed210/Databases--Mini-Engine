e894a163d195e22c9610751fce35355b81c92b82
eeeee <hazemanwar349@gmail.com>
2023-03-18 23:30:38 +0200

    first commit

---
+++ DBApp/src/engine/DBAppException.java
@@ -0,0 +1,11 @@
+package engine;
+
+@SuppressWarnings("serial")
+public class DBAppException extends Exception {
+
+	public DBAppException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
